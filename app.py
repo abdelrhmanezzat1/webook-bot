b279faa83ed443618026cea02f931a4645177106
abdelrhmanezzat1 <90967671+abdelrhmanezzat1@users.noreply.github.com>
2026-05-06 16:33:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from flask import Flask, render_template, request, jsonify
 from flask_socketio import SocketIO, emit
 import random
 import logging
+import os
 
 app = Flask(__name__)
 app.secret_key = 'webook_bot_super_secret'
@@ -350,4 +351,5 @@ def handle_connect():
     emit('update_accounts', accounts)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host='0.0.0.0', port=port)
